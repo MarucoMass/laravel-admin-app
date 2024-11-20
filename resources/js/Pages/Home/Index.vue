8c6507d1640011d30111f234fa9f8dc6ec17f41a
MarucoMass <mariomass23@gmail.com>
2024-11-20 14:46:44 -0300

    dashboard layout

--- resources/js/Pages/Home/Index.vue
+++ resources/js/Pages/Home/Index.vue
@@ -1,15 +1,15 @@
 <script setup>
 // import Layout from './Layout'
 import { Head } from '@inertiajs/vue3'
-
+import Layout from '../../Layout/App.vue'
 // defineProps({ user: Object })
 </script>
 
 <template>
-    <!-- <Layout> -->
+    <Layout>
 
         <Head title="Welcome" />
         <h1 class="text-3xl text-red-600">Welcome</h1>
         <!-- <p>Hello {{ user.name }}, welcome to your first Inertia app!</p> -->
-    <!-- </Layout> -->
+    </Layout>
 </template>
